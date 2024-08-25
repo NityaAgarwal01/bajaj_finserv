391a09ceb40a9e199efc5c8f9679aaa72337919f
NityaAgarwal01 <154603160+NityaAgarwal01@users.noreply.github.com>
2024-08-25 11:53:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app
 
     res.json({
       is_success: true,
-      user_id: "sanhita17",
-      email: "sanhita.kundu2020@vitstudent.ac.in",
-      roll_number: "20BEC0215",
+      user_id: "nitya_10",
+      email: "nitya.agarwal2021@vitstudent.ac.in",
+      roll_number: "21BIT0238",
       numbers: numbers,
       alphabets: alphabets,
       highest_alphabet: highest_alphabet ? [highest_alphabet] : [],
